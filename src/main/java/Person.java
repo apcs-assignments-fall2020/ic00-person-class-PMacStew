3bf27fd228aa5153985288d3ef0c4d55db5fc668
PMacStew <23pmcclure@athenian.org>
2020-10-27 18:52:55 +0000

    finish

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,5 +1,33 @@
 public class Person {
     
+    private String firstName;
+    private String lastName;
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    public String chill() {
+        return "It's me, " + this.firstName + " " + this.lastName + ". Just chillin with my homies";
+    }
+    public String getFirstName() {
+        return this.firstName;
+    }
+    public void setFirstName(String newName) {
+        this.firstName = newName;
+    }
+    public String getLastName() {
+        return this.lastName;
+    }
+    public void setLastName(String newName) {
+        if (newName.trim().length() != 0)
+            this.lastName = newName;
+    }
+
+    @Override
+    public String toString() {
+        return this.firstName + " " + this.lastName;
+    }
 }
 
 
--- src/main/java/PersonClient.java
+++ src/main/java/PersonClient.java
@@ -1,5 +1,7 @@
 public class PersonClient {
     public static void main(String[] args) {
-        
+        Person phil = new Person("Phillip", "McClure");
+        System.out.println(phil);
+        System.out.println(phil.chill());
     }
 }
